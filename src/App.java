0dec9cc9e4bdf278efca95af5b7df6a69666fce6
matexss <welin0371734d@gmail.com>
2025-04-23 16:56:53 -0500

    Finalizacion de la prueba

--- src/App.java
+++ src/App.java
@@ -1,38 +1,44 @@
+
+import Controllers.libroController;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Book[] books = {
-                new Book("Clean Code", 2008),
-                new Book("The Pragmatic Programmer", 1999),
-                new Book("Design Patterns: Elements of Reusable Object-Oriented Software", 1994),
-                new Book("Refactoring: Improving the Design of Existing Code", 1999),
-                new Book("Effective Java", 2001),
-                new Book("You Don't Know JS: Scope & Closures", 2014),
-                new Book("Introduction to the Theory of Computation", 1997),
-                new Book("Code: The Hidden Language of Computer Hardware and Software", 2000),
-                new Book("Structure and Interpretation of Computer Programs", 1985),
-                new Book("The Mythical Man-Month", 1975),
-                new Book("Head First Design Patterns", 2004),
-                new Book("Programming Pearls", 1986),
-                new Book("Algorithms", 1983),
-                new Book("Java Concurrency in Practice", 2006),
-                new Book("The Art of Computer Programming", 1968),
-                new Book("Computer Networking: A Top-Down Approach", 2000),
-                new Book("Artificial Intelligence: A Modern Approach", 1995),
-                new Book("Compilers: Principles, Techniques, and Tools", 1986),
-                new Book("Operating System Concepts", 1983),
-                new Book("Database System Concepts", 1986),
-                new Book("The C Programming Language", 1978),
-                new Book("Introduction to Algorithms", 1990),
-                new Book("The Linux Programming Interface", 2010),
-                new Book("Python Crash Course", 2015),
-                new Book("Cracking the Coding Interview", 2008),
-                new Book("The Go Programming Language", 2015),
-                new Book("Modern Operating Systems", 1992),
-                new Book("Computer Organization and Design", 1994),
-                new Book("Deep Learning", 2016),
-                new Book("The Elements of Statistical Learning", 2001)
+        book[] books = {
+                new book("Clean Code", 2008),
+                new book("The Pragmatic Programmer", 1999),
+                new book("Design Patterns: Elements of Reusable Object-Oriented Software", 1994),
+                new book("Refactoring: Improving the Design of Existing Code", 1999),
+                new book("Effective Java", 2001),
+                new book("You Don't Know JS: Scope & Closures", 2014),
+                new book("Introduction to the Theory of Computation", 1997),
+                new book("Code: The Hidden Language of Computer Hardware and Software", 2000),
+                new book("Structure and Interpretation of Computer Programs", 1985),
+                new book("The Mythical Man-Month", 1975),
+                new book("Head First Design Patterns", 2004),
+                new book("Programming Pearls", 1986),
+                new book("Algorithms", 1983),
+                new book("Java Concurrency in Practice", 2006),
+                new book("The Art of Computer Programming", 1968),
+                new book("Computer Networking: A Top-Down Approach", 2000),
+                new book("Artificial Intelligence: A Modern Approach", 1995),
+                new book("Compilers: Principles, Techniques, and Tools", 1986),
+                new book("Operating System Concepts", 1983),
+                new book("Database System Concepts", 1986),
+                new book("The C Programming Language", 1978),
+                new book("Introduction to Algorithms", 1990),
+                new book("The Linux Programming Interface", 2010),
+                new book("Python Crash Course", 2015),
+                new book("Cracking the Coding Interview", 2008),
+                new book("The Go Programming Language", 2015),
+                new book("Modern Operating Systems", 1992),
+                new book("Computer Organization and Design", 1994),
+                new book("Deep Learning", 2016),
+                new book("The Elements of Statistical Learning", 2001)
         };
+            libroController lC = new libroController();
+
+            
 
     }
 }
---
+++ src/Controllers/LibroController.java
@@ -0,0 +1,18 @@
+package Controllers;
+import Models.book;
+
+public class libroController {
+    shortByName(Book[] libros){
+        for(int i = 0 ; i < arr.length -1 ; i++){
+            for(int j = 0; j< arr.length -i -1; j++){
+                if(get.Name[j] < get.Name[j+1]);
+
+                int temp = get.Name[j];
+                get.Name[j] = get.Name[j+1];
+                get.Name[j+1] = temp;
+            }
+        }
+    }
+    searchByName(Book[] libros, String nombre)
+    
+}
